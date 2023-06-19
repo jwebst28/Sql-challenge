5f5d9bb579ed56cd6e42474a0e96939cb6730c96
jwebst28 <126523999+jwebst28@users.noreply.github.com>
2023-06-19 19:31:39 -0400

    QuickDBD-Diagram

---
+++ QuickDBD-Free Diagram (1).sql
@@ -0,0 +1,96 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/7uqoVQ
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+CREATE TABLE [titles] (
+    [title_id] VARCHAR  NOT NULL ,
+    [title] VARCHAR(30)  NOT NULL ,
+    CONSTRAINT [PK_titles] PRIMARY KEY CLUSTERED (
+        [title_id] ASC
+    )
+)
+
+CREATE TABLE [employees] (
+    [emp_no] INTEGER  NOT NULL ,
+    [emp_title_id] VARCHAR(10)  NOT NULL ,
+    [birth_date] DATE  NOT NULL ,
+    [first_name] VARCHAR  NOT NULL ,
+    [last_name] VARCHAR  NOT NULL ,
+    [sex] VARCHAR  NOT NULL ,
+    [hire_date] DATE  NOT NULL ,
+    CONSTRAINT [PK_employees] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+CREATE TABLE [Departments] (
+    [dept_no] VARCHAR(10)  NOT NULL ,
+    [dept_name] VARCHAR(30)  NOT NULL ,
+    CONSTRAINT [PK_Departments] PRIMARY KEY CLUSTERED (
+        [dept_no] ASC
+    )
+)
+
+CREATE TABLE [dept_manager] (
+    [dept_no] VARCHAR(10)  NOT NULL ,
+    [emp_no] INTEGER  NOT NULL ,
+    CONSTRAINT [PK_dept_manager] PRIMARY KEY CLUSTERED (
+        [dept_no] ASC,[emp_no] ASC
+    )
+)
+
+CREATE TABLE [dept_emp] (
+    [emp_no] INTEGER  NOT NULL ,
+    [dept_no] VARCHAR(10)  NOT NULL ,
+    CONSTRAINT [PK_dept_emp] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC,[dept_no] ASC
+    )
+)
+
+CREATE TABLE [salaries] (
+    [emp_no] INTEGER  NOT NULL ,
+    [salary] INTEGER  NOT NULL ,
+    CONSTRAINT [PK_salaries] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+ALTER TABLE [employees] WITH CHECK ADD CONSTRAINT [FK_employees_emp_title_id] FOREIGN KEY([emp_title_id])
+REFERENCES [titles] ([title_id])
+
+ALTER TABLE [employees] CHECK CONSTRAINT [FK_employees_emp_title_id]
+
+ALTER TABLE [dept_manager] WITH CHECK ADD CONSTRAINT [FK_dept_manager_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [Departments] ([dept_no])
+
+ALTER TABLE [dept_manager] CHECK CONSTRAINT [FK_dept_manager_dept_no]
+
+ALTER TABLE [dept_manager] WITH CHECK ADD CONSTRAINT [FK_dept_manager_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [dept_manager] CHECK CONSTRAINT [FK_dept_manager_emp_no]
+
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_emp_no]
+
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [Departments] ([dept_no])
+
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_dept_no]
+
+ALTER TABLE [salaries] WITH CHECK ADD CONSTRAINT [FK_salaries_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [salaries] CHECK CONSTRAINT [FK_salaries_emp_no]
+
+COMMIT TRANSACTION QUICKDBD
